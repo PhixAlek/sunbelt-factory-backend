4109abec16aedc0ed40fea090fbb38060abe6e8d
Alek1l99 <miguelsegura618@gmail.com>
2024-07-20 02:09:54 -0500

    Project Setup

---
+++ src/main/java/com/sunbelt_factory/consult_data/SunbeltConsultDataApplication.java
@@ -0,0 +1,13 @@
+package com.sunbelt_factory.consult_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunbeltConsultDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunbeltConsultDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunbelt_factory/consult_data/SunbeltConsultDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunbelt_factory.consult_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunbeltConsultDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
